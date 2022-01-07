e9be85125d2cc23a7b544449e857d843cc3c298e
Lyokolux <lyokolux@protonmail.com>
2022-01-07 11:44:06 +0100

    Fix MarkerDescription warnings

--- app/src/main/java/com/gooutinmetz/map/MarkerDescription.java
+++ app/src/main/java/com/gooutinmetz/map/MarkerDescription.java
@@ -5,25 +5,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
 import com.gooutinmetz.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 
 public class MarkerDescription implements GoogleMap.InfoWindowAdapter {
     private View markerModal = null;
-    private LayoutInflater inflater;
+    private final LayoutInflater inflater;
 
     public MarkerDescription(LayoutInflater inflater) {
         this.inflater = inflater;
     }
 
     @Override
-    public View getInfoWindow(Marker marker) {
+    public View getInfoWindow(@NonNull Marker marker) {
         return null;
     }
 
+    @SuppressLint("InflateParams")
     @Override
-    public View getInfoContents(Marker marker) {
+    public View getInfoContents(@NonNull Marker marker) {
         if (markerModal == null)
             markerModal = inflater.inflate(R.layout.marker_description, null);
 
