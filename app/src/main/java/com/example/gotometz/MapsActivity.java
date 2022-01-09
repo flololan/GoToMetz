592589950cca8b1beddb888e6d4fb335006797f8
johannchopin <johannchopin@pm.me>
2022-01-09 13:15:04 +0100

    refactor

--- app/src/main/java/com/example/gotometz/MapsActivity.java
+++ app/src/main/java/com/example/gotometz/MapsActivity.java
@@ -36,6 +36,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -53,10 +54,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private SiteService siteDao;
 
     LocationManager locationManager = null;
-    private String provider;
-    //MyLocationListener myLocationListener; // location listener
 
-    // Used when doing a site research
     private Category searchedCategory;
     private int searchedRadius;
 
@@ -65,51 +63,43 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public Button addSiteBTN;
 
     public String bestProvider;
-    public Criteria criteria;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Vérifie les permissions
-        permissions = 0;
+        if (this.permissionsAreValid())
+            run();
+    }
+
+    private Boolean permissionsAreValid() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             //Permission de localisation
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         } else {
-            permissions++;
-
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     != PackageManager.PERMISSION_GRANTED) {
                 //Permission pour écrire dans le fichier
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
             } else {
-                permissions++;
-
                 if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
                     //Permission pour lire le fichier
                     ActivityCompat.requestPermissions(this,
                             new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
-                }
-                else
-                    permissions++;
+                } else
+                    return true;
             }
         }
 
-        //Lance l'application quand toutes les permissions sont accordées
-        if (permissions == 3)
-            run();
+        return false;
     }
 
-    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        //Affiche un message si la permission n'est pas accordée
         Toast toast = Toast.makeText(this, this.getResources().getString(R.string.noPermissions), Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_DENIED)
@@ -121,11 +111,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
-            //Si la permissionj d'écrire dans le fichier est accordée
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
-        } else
-        if (requestCode != 3)
+        } else if (requestCode != 3)
             MapsActivity.permissions++;
 
         if (MapsActivity.permissions == 2) {
@@ -146,9 +134,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_maps);
 
         //initialisation du menu
-        BottomNavigationView bottomNavigationView = this.findViewById(R.id.menu);
-        bottomNavigationView.setSelectedItemId(R.id.mapMenu);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new Menu(this));
+        this.initMenu();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -156,11 +142,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (mapFragment != null)
             mapFragment.getMapAsync(this);
 
-        //récupérer les données bdd via la DAO
-        categoryDao = CategoryService.getInstance(this);
-        siteDao = SiteService.getInstance(this);
+        this.initDAO();
 
-        // Bouton listeners
+        // listeners
 
         Button searchSiteFromUserBTN = findViewById(R.id.searchSiteFromUserBTN_);
         searchSiteFromUserBTN.setOnClickListener(new SearchSiteListener(this));
@@ -172,22 +156,22 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         addSiteBTN = findViewById(R.id.addSiteBTN_);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private void initMenu() {
+        BottomNavigationView bottomNavigationView = this.findViewById(R.id.menu);
+        bottomNavigationView.setSelectedItemId(R.id.mapMenu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new Menu(this));
+    }
+
+    private void initDAO() {
+        categoryDao = CategoryService.getInstance(this);
+        siteDao = SiteService.getInstance(this);
+    }
+
     @SuppressLint("MissingPermission")
-    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        //Récupère la position de l'utilisateur
-        locationInitialization();
+        this.initLocation();
 
         mMap.setMyLocationEnabled(true);
 
@@ -203,23 +187,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         this.myMapListener = new MyMapListener(this);
     }
 
-    @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        //Ajouter un site
         if (data != null && data.getExtras() != null)
             if (data.getLongExtra("id", -1) == -1) {
                 Site site = new Site(data.getStringExtra("label"), data.getDoubleExtra("latitude", 0),
                         data.getDoubleExtra("longitude", 0), data.getStringExtra("postalAddress"),
                         categoryDao.findById(data.getLongExtra("categoryId", -1)), data.getStringExtra("summary"));
 
-                //Création du site dans la bdd
                 site.setId(siteDao.create(site));
             }
     }
 
-    @Override
     public void onDialogPositiveClick(Location location, Category category, int radius) {
         searchedCategory = category;
         searchedRadius = radius;
@@ -231,67 +211,77 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void searchSites(Location location) {
         if (searchedCategory != null) {
             mMap.clear();
-
-            //Ajouter le cercle sur la map
-            mMap.addCircle(new CircleOptions()
-                    .center(new LatLng(location.getLatitude(), location.getLongitude()))
-                    .radius(searchedRadius)
-                    .strokeColor(Color.BLACK)
-                    .fillColor(Color.argb(0.3f, 0, 0, 255)));
-
-            //Ajouter les marqueurs sur la map
-            Location siteLocation = new Location("");
-            LatLng siteLatLng;
-            double siteDistance;
-            for (Site site : siteDao.findByCategory(searchedCategory)) {
-                siteLocation.setLatitude(site.getLatitude());
-                siteLocation.setLongitude(site.getLongitude());
-
-                siteLatLng = new LatLng(site.getLatitude(), site.getLongitude());
-
-                siteDistance = location.distanceTo(siteLocation);
-                siteDistance = Math.round(siteDistance * 100) / 100.0;
-
-                //Vérifier si le marqueurs du site se situe dans la rayon de recherche
-                if (siteDistance <= searchedRadius)
-                    mMap.addMarker(new MarkerOptions()
-                            .position(siteLatLng)
-                            .title(site.getLabel())
-                            .snippet(site.getSummary() + "\n" + getString(R.string.distance) + " " + siteDistance + " m"));
-            }
+            this.renderMarkersOnMap(location);
+            this.renderMarkersOnMap(location);
         }
     }
 
-    @SuppressLint("MissingPermission")
-    private void locationInitialization() {
+    private void renderSearchAreaOnMap(Location location) {
+        mMap.addCircle(new CircleOptions()
+                .center(new LatLng(location.getLatitude(), location.getLongitude()))
+                .radius(searchedRadius)
+                .strokeColor(Color.BLACK)
+                .fillColor(Color.argb(0.3f, 0, 0, 255)));
+    }
 
+    private void renderMarkersOnMap(Location location) {
+        Location siteLocation = new Location("");
+        LatLng siteLatLng;
+        double siteDistance;
+        for (Site site : siteDao.findByCategory(searchedCategory)) {
+            siteLocation.setLatitude(site.getLatitude());
+            siteLocation.setLongitude(site.getLongitude());
 
-            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
-            bestProvider = LocationManager.NETWORK_PROVIDER;
+            siteLatLng = new LatLng(site.getLatitude(), site.getLongitude());
+
+            siteDistance = location.distanceTo(siteLocation);
+            siteDistance = Math.round(siteDistance * 100) / 100.0;
+
+            Boolean isMarkerLocatedInSearchRadius = siteDistance <= searchedRadius;
+
+            if (isMarkerLocatedInSearchRadius)
+                mMap.addMarker(new MarkerOptions()
+                        .position(siteLatLng)
+                        .title(site.getLabel())
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                        .snippet(site.getSummary() + "\n" + getString(R.string.distance) + " " + siteDistance + " m"));
+        }
+    }
+
+    public MapsActivity(int contentLayoutId) {
+        super(contentLayoutId);
+    }
+
+    public MapsActivity() {
+        super();
+    }
+
+    private void initLocation() {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        bestProvider = LocationManager.NETWORK_PROVIDER;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
+
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(true);
+        criteria.setBearingRequired(true);
+        criteria.setSpeedRequired(true);
+        criteria.setCostAllowed(true);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+
+        setUserLocation(locationManager.getLastKnownLocation(bestProvider));
+
+        if (userLocation != null)
+           this.onLocationChanged(userLocation);
+        else{
             locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
-
-            criteria.setAccuracy(Criteria.ACCURACY_FINE);
-            criteria.setAltitudeRequired(true);
-            criteria.setBearingRequired(true);
-            criteria.setSpeedRequired(true);
-            criteria.setCostAllowed(true);
-            criteria.setPowerRequirement(Criteria.POWER_HIGH);
-
-
-            setUserLocation(locationManager.getLastKnownLocation(bestProvider));
-
+            Log.e("Erreur Localisation","Entrer dans else de requestLocationUpdates");
             if (userLocation != null)
-               this.onLocationChanged(userLocation);
-            else{
-                locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
-                Log.e("Erreur Localisation","Entrer dans else de requestLocationUpdates");
-                if (userLocation != null)
-                    this.onLocationChanged(userLocation);
-            }
-
-
-
+                this.onLocationChanged(userLocation);
+        }
     }
 
     public Location getUserLocation() {
@@ -302,28 +292,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         this.userLocation = userLocation;
     }
 
-    @Override
     public void onLocationChanged(@NonNull Location location) {
         this.setUserLocation(location);
         this.searchSites(location);
-
-    }
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
     }
 
-    @Override
-    public void onProviderEnabled(String provider) {
+    public void onStatusChanged(String provider, int status, Bundle extras) { }
 
-    }
+    public void onProviderEnabled(String provider) { }
 
-    @Override
-    public void onProviderDisabled(String provider) {
+    public void onProviderDisabled(String provider) { }
 
-    }
-
-    public void searchNearestPlace(String v2txt) {
-        //.....
-    }
+    public void searchNearestPlace(String v2txt) { }
 }
